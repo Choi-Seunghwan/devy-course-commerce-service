079d052e78ce62439c6285484344251417f275ab
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-03-04 22:54:58 +0900

    feat: order, payment 모듈 생성

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
 import { ProductModule } from './product/product.module';
+import { OrderModule } from './order/order.module';
+import { PaymentModule } from './payment/payment.module';
 
 @Module({
-  imports: [DatabaseModule, ProductModule],
+  imports: [DatabaseModule, ProductModule, OrderModule, PaymentModule],
   controllers: [AppController],
   providers: [AppService],
 })
