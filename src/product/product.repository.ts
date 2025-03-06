1ec814e95274100aa46572bff6b12598cdc6567b
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-03-07 00:08:45 +0900

    feat: api-util 모듈 추가

--- src/product/product.repository.ts
+++ src/product/product.repository.ts
@@ -6,16 +6,27 @@ import { PrismaService } from 'src/database/prisma.service';
 export class productRepository {
   constructor(private readonly prisma: PrismaService) {}
 
+  private getOmit() {
+    return {
+      createdById: true,
+      updateById: true,
+      deleteById: true,
+      deletedAt: true,
+    };
+  }
+
   async findAll(args: Prisma.ProductFindManyArgs) {
     return await this.prisma.product.findMany({
       ...args,
       where: { ...args.where, deletedAt: null },
+      omit: this.getOmit(),
     });
   }
 
   async findById(id: number) {
     return await this.prisma.product.findUnique({
       where: { id, deletedAt: null },
+      omit: this.getOmit(),
     });
   }
 }
