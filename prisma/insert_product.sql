4d98f73fa2a2a2787fd49a2686e6e1a478ecf5c9
Choi-Seunghwan <choiseunghwan.tech@gmail.com>
2025-04-01 12:52:26 +0900

    feat: update insert query

--- prisma/insert_product.sql
+++ prisma/insert_product.sql
@@ -1,63 +1,73 @@
 INSERT INTO
-  product (name, price, description, created_by_id)
+  product (name, price, description, images, created_by_id)
 VALUES
   (
     '프리미엄 오버핏 베이직 티셔츠',
     29000,
     '트렌디한 오버핏 실루엣과 부드러운 코튼 소재로 데일리룩의 완성! 다양한 컬러 구성으로 원하는 스타일을 연출할 수 있으며, 세탁 후에도 변형 없이 오래도록 유지됩니다.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/35510c29-e385-4758-ad99-1c1deb8a5a5b.png'],
     1
   ),
   (
     '클래식 슬림핏 블레이저 자켓',
     89000,
     '세련된 슬림핏 라인으로 격식 있는 자리부터 캐주얼한 스타일링까지 완벽하게 소화하는 머스트 해브 아이템! 고급스러운 원단과 정교한 봉제로 완성된 클래식 블레이저 자켓.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/4721d07e-9c94-4765-9487-48737f0b65a6.png'],
     1
   ),
   (
     '소프트 터치 니트 가디건',
     45000,
     '포근한 촉감과 부드러운 착용감으로 가을, 겨울 데일리룩에 딱! 심플한 디자인과 적당한 두께감으로 다양한 스타일링이 가능하며, 가볍게 걸쳐도 멋스러운 분위기를 연출할 수 있는 필수 아이템.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/ab7f0a3a-2f8c-4e6d-941a-fe2ffbef5a9c.png'],
     1
   ),
   (
     '하이웨이스트 와이드 데님 팬츠',
     59000,
     '다리가 길어 보이는 효과는 물론, 편안한 핏까지! 어디에나 잘 어울리는 클래식한 데님 컬러와 감각적인 핏으로, 어떤 상의와도 완벽한 조화를 이루는 스타일리시한 데님 팬츠.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/086c7bd3-acf0-497f-a7dd-a06af312c0d1.png'],
     1
   ),
   (
     '14K 골드 미니 하트 목걸이',
     120000,
     '심플하면서도 우아한 포인트가 되어줄 14K 골드 미니 하트 목걸이! 데일리로 착용하기 좋은 베이직한 디자인으로, 특별한 날에도 부담 없이 즐길 수 있는 고급스러운 아이템.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/0008dcc8-7964-4ca7-8a9a-aab68a4aef2c.png'],
     1
   ),
   (
     '실버 큐빅 테니스 팔찌',
     75000,
     '세련된 실버 체인과 반짝이는 큐빅이 조화롭게 어우러진 테니스 팔찌! 어떤 룩에도 자연스럽게 어울리며, 여성스러운 무드를 한층 더해주는 특별한 액세서리로 추천드립니다.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/d54e9f9c-56fe-40df-aeb7-80c63217ab11.png'],
     1
   ),
   (
     '925 실버 트위스트 링',
     49000,
     '베이직하면서도 감각적인 트위스트 디자인이 돋보이는 925 실버 반지! 단독으로 착용해도 좋고, 다른 반지들과 레이어드해서 스타일링해도 멋스러운 데일리 아이템입니다.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/7f8331d8-3c62-486b-bdfb-f61665f2000f.png'],
     1
   ),
   (
     '울 블렌드 체크 롱코트',
     130000,
     '한겨울까지 따뜻하게, 세련된 체크 패턴과 클래식한 롱코트 실루엣으로 스타일과 보온성을 동시에 잡은 제품! 어떤 룩에도 멋스럽게 어우러지는 감각적인 디자인이 매력적인 아이템입니다.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/ddddf377-0c04-448f-a276-87ef748046c9.png'],
     1
   ),
   (
     '진주 드롭 이어링',
     69000,
     '우아한 무드를 연출해주는 클래식한 진주 드롭 이어링! 깔끔한 디자인과 자연스러운 광택이 돋보이며, 어느 스타일에도 은은한 포인트를 더해주는 고급스러운 주얼리입니다.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/730e3a99-aec8-47c2-963c-2aef7e33e9c3.png'],
     1
   ),
   (
     '모던 레더 숄더백',
     99000,
     '데일리로 부담 없이 착용하기 좋은 미니멀한 디자인의 레더 숄더백! 깔끔한 쉐입과 실용적인 수납 공간을 갖춘 아이템으로, 어떤 스타일에도 완벽하게 어울리는 머스트 해브 백입니다.',
+    ARRAY['https://commerce-shop-assets.s3.ap-northeast-2.amazonaws.com/products/product1.png'],
     1
   );
